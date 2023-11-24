4301a4aa13686087391011770ed8cfc72d18a774
Aicanaro <97106925+Aicanaro@users.noreply.github.com>
2023-11-24 18:32:44 +0500

    Update method .add

---
+++ ListImpl/src/main/java/KARTA.java
@@ -0,0 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class KARTA<K,V> implements IMap<K,V>{
+
+
+
+    private ArrayList<K> keys;
+    private ArrayList<V> values;
+
+    public KARTA(){
+
+    }
+    @Override
+    public void add(K key, V value) {
+        keys.add(key);
+        values.add(value);
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public Set<K> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(V value) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}
