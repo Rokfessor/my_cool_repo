009e33e82181060f4d34bb68db0134b1e4c4b5a3
ibofh <kartasevm9@gmail.com>
2023-12-08 18:29:16 +0500

    bugFix for add

--- ListImpl/src/main/java/AdMap.java
+++ ListImpl/src/main/java/AdMap.java
@@ -1,30 +1,40 @@
 import java.util.ArrayList;
 import java.util.Set;
 
-public class AdMap<K,V> implements IMap<K,V>{
+public class AdMap<K, V> implements IMap<K, V> {
     private ArrayList<MapObj> listOfElements = new ArrayList<>();
     private MapObj<K, V> slider;
-    public AdMap(){
+    private int indexForRemove;
+
+    public AdMap() {
 
     }
+
     @Override
     public void add(K key, V value) {
-        listOfElements.add(new MapObj<K, V>(key, value));
-    }
-    @Override
-    public V get(K key) {
-        for(int i = 0; i < listOfElements.size(); i++){
-            slider = listOfElements.get(i);
-            if(slider.getKey() == key){
-                return slider.getValue();
+        if (listOfElements.size() > 0) {
+            for (int i = 0; i < listOfElements.size(); i++) {
+                if (listOfElements.get(i).getKey() == key) {
+                    indexForRemove = i;
+                }
+            }
+            if (indexForRemove >= 0) {
+                listOfElements.remove(indexForRemove);
             }
         }
-        return null;
+        listOfElements.add(new MapObj<K, V>(key, value));
     }
+
     @Override
-    public Set<K> getAll(){
+    public V get(K key) {
         return null;
     }
+
+    @Override
+    public Set<K> getAll() {
+        return null;
+    }
+
     @Override
     public boolean remove(K key) {
         return false;
