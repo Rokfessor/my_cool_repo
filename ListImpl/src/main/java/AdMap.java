7e91616af862c1fb3ac042d8d60ece637d2ba50e
ibofh <kartasevm9@gmail.com>
2023-12-15 18:00:17 +0500

    removing indexForRemove + add via remove

--- ListImpl/src/main/java/AdMap.java
+++ ListImpl/src/main/java/AdMap.java
@@ -4,7 +4,6 @@ import java.util.Set;
 
 public class AdMap<K, V> implements IMap<K, V> {
     private ArrayList<MapObj> listOfElements = new ArrayList<>();
-    private int indexForRemove;
     Set<K> setForGetAll = new HashSet<>();
     ArrayList<Integer> listOfIndexForRemoveAll = new ArrayList<>();
 
@@ -15,16 +14,8 @@ public class AdMap<K, V> implements IMap<K, V> {
     @Override
     public void add(K key, V value) {
         if (listOfElements.size() > 0) {
-            for (int i = 0; i < listOfElements.size(); i++) {
-                if (listOfElements.get(i).getKey() == key) {
-                    indexForRemove = i;
-                }
-            }
-            if (indexForRemove >= 0) {
-                listOfElements.remove(indexForRemove);
-            }
+            remove(key);
         }
-        indexForRemove = -1;
         listOfElements.add(new MapObj<K, V>(key, value));
         setForGetAll.add(key);
     }
@@ -50,19 +41,13 @@ public class AdMap<K, V> implements IMap<K, V> {
         if (listOfElements.size() > 0) {
             for (int i = 0; i < listOfElements.size(); i++) {
                 if (listOfElements.get(i).getKey() == key) {
-                    indexForRemove = i;
+                    listOfElements.remove(i);
+                    break;
                 }
             }
-            if (indexForRemove >= 0) {
-                listOfElements.remove(indexForRemove);
-                setForGetAll.remove(key);
-                return true;
-            }
         }
-        indexForRemove = -1;
         return false;
     }
-
     @Override
     public boolean removeAll(V value) {
         if (listOfElements.size() > 0) {
